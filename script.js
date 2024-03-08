dd24de10f52c44d9544bfe44a30c16a3c55558cd
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2024-03-08 07:30:35 +0000

    commit

--- script.js
+++ script.js
@@ -37,5 +37,7 @@ function handleClick(e){
     const cell = e.target
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
     placeMark(cell, currentClass)
-    
+    if (checkWin(currentClass)){
+        endGame(false)
+    } else if 
 }
